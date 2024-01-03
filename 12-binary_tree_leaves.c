f0c815e14cac637ab08a428ce8fa84a09b78c421
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-03 01:42:18 -0800

    Task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Func counts leaves in binary tree.
+ *
+ * @tree: Pointer to root node of tree to count leaves of.
+ *
+ * Return: The number of leaves in the tree.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree)
+	{
+		bin_leaves += (!tree->left && !tree->right) ? 1 : 0;
+		bin_leaves += binary_tree_leaves(tree->left);
+		bin_leaves += binary_tree_leaves(tree->right);
+	}
+	return (bin_leaves);
+}

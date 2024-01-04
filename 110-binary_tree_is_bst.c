081072c02d23f18febd0550280f30736c692ca6e
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-04 03:47:06 -0800

    Task 24 - Advanced - Betty issues

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,14 +2,14 @@
 #include "limits.h"
 
 /**
- * is_bst_helper - Func checks if binary tree is valid binary search tree.
- *
- * @tree: Pointer to root node of binary tree to check.
- * @lo: Value of smallest node in current search.
- * @hi: Value of largest node in current search.
- *
- * Return: If the tree is a valid BST (1), otherwise return 0.
- */
+ * is_bst_helper - Func checks if binary tree is valid binary search tree.
+ *
+ * @tree: Pointer to root node of binary tree to check.
+ * @lo: Value of smallest node in current search.
+ * @hi: Value of largest node in current search.
+ *
+ * Return: If the tree is a valid BST (1), otherwise return 0.
+ */
 int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 {
 	if (tree != NULL)
@@ -19,18 +19,18 @@ int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 			return (0);
 		}
 		return (is_bst_helper(tree->left, lo, tree->n - 1) &&
-			is_bst_helper(tree->right, tree->n + 1, hi));
+				is_bst_helper(tree->right, tree->n + 1, hi));
 	}
 	return (1);
 }
 
 /**
- * binary_tree_is_bst - Func checks if binary tree is valid BST.
- *
- * @tree: Pointer to root node of tree to check.
- *
- * Return: If the tree is a valid BST (1), otherwise return 0.
- */
+ * binary_tree_is_bst - Func checks if binary tree is valid BST.
+ *
+ * @tree: Pointer to root node of tree to check.
+ *
+ * Return: If the tree is a valid BST (1), otherwise return 0.
+ */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
